154004e14163bd81a1c5cc8ad74b6c3a7f93db63
rgaynetdinov <foxcoolman@byndyusoft.com>
2023-04-10 10:55:33 +0500

    Проверка даты на тип DateTimeOffset

--- exmaple/Byndyusoft.DotNet.Testing.Infrastructure.Examples/CheckAllDateTimesBeClosedToExamplesTests.cs
+++ exmaple/Byndyusoft.DotNet.Testing.Infrastructure.Examples/CheckAllDateTimesBeClosedToExamplesTests.cs
@@ -53,5 +53,48 @@ namespace Byndyusoft.Byndyusoft.DotNet.Testing.Infrastructure.Examples
 
             // Assert
         }
+
+        [Fact]
+        public void CheckAllDateTimesBeClosedTo_AllDateTimeOffsetCloseToEachOtherWith5seconds_ShouldPass()
+        {
+            // Arrange
+            var actual = new TestEntityWithDateTimeOffset
+            {
+                                 CreateDate = DateTimeOffset.UtcNow,
+                                 UpdateDate = DateTimeOffset.UtcNow
+                             };
+            var expected = new TestEntityWithDateTimeOffset
+            {
+                                   CreateDate = DateTimeOffset.UtcNow.AddSeconds(4),
+                                   UpdateDate = DateTimeOffset.UtcNow.AddSeconds(-5)
+                               };
+
+            // Act
+            actual.Should().BeEquivalentTo(expected, options => options.CheckAllDateTimesBeClosedTo(TimeSpan.FromSeconds(5)));
+
+            // Assert
+        }
+
+        [Fact(Skip = "Этот тест должен падать")]
+        public void CheckAllDateTimesBeClosedTo__AllDateTimeOffsetNotCloseToEachOtherWith5seconds_ShouldFail()
+        {
+            // Arrange
+            var actual = new TestEntityWithDateTimeOffset
+                             {
+                                 CreateDate = DateTimeOffset.UtcNow,
+                                 UpdateDate = DateTimeOffset.UtcNow
+                             };
+            var expected = new TestEntityWithDateTimeOffset
+            {
+                                   CreateDate = DateTimeOffset.UtcNow.AddSeconds(10),
+                                   UpdateDate = DateTimeOffset.UtcNow.AddSeconds(-10)
+                               };
+
+
+            // Act
+            actual.Should().BeEquivalentTo(expected, options => options.CheckAllDateTimesBeClosedTo(TimeSpan.FromSeconds(5)));
+
+            // Assert
+        }
     }
 }
--- src/Byndyusoft.DotNet.Testing.Infrastructure/Extensions/EquivalencyAssertionOptionsExtensions.cs
+++ src/Byndyusoft.DotNet.Testing.Infrastructure/Extensions/EquivalencyAssertionOptionsExtensions.cs
@@ -16,19 +16,22 @@ namespace Byndyusoft.DotNet.Testing.Infrastructure.Extensions
             this EquivalencyAssertionOptions<TType> options)
         {
             return options.Using<int>(context => context.Subject.Should().BePositive())
-                .When(info => info.SelectedMemberInfo != null && info.SelectedMemberInfo.Name == "Id");
+                          .When(info => info.SelectedMemberInfo != null && info.SelectedMemberInfo.Name == "Id");
         }
 
         /// <summary>
-        ///     Проверяет, что в проверяемой сущности все значения типа DateTime находятся в пределах определённого интервала от
-        ///     заданного момента времени
+        ///     Проверяет, что в проверяемой сущности все значения типа DateTime или DateTimeOffset находятся в пределах
+        ///     определённого интервала от заданного момента времени
         /// </summary>
         public static EquivalencyAssertionOptions<TType> CheckAllDateTimesBeClosedTo<TType>(
-            this EquivalencyAssertionOptions<TType> options, TimeSpan precision)
+            this EquivalencyAssertionOptions<TType> options,
+            TimeSpan precision)
         {
             return options
-                .Using<DateTime>(context => context.Subject.Should().BeCloseTo(context.Expectation, precision))
-                .WhenTypeIs<DateTime>();
+                   .Using<DateTime>(context => context.Subject.Should().BeCloseTo(context.Expectation, precision))
+                   .WhenTypeIs<DateTime>()
+                   .Using<DateTimeOffset>(context => context.Subject.Should().BeCloseTo(context.Expectation, precision))
+                   .WhenTypeIs<DateTimeOffset>();
         }
     }
 }
